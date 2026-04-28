2ef604f799459bfea21ae0f40673edba52be7f9f
chenwanchang <2031411561@qq.com>
2026-04-28 17:29:15 +0800

    Update system_setting_old.go

--- setting/system_setting/system_setting_old.go
+++ setting/system_setting/system_setting_old.go
@@ -1,6 +1,6 @@
 package system_setting
 
-var ServerAddress = "http://localhost:3000"
+var ServerAddress = "https://tokenparty.org"
 var WorkerUrl = ""
 var WorkerValidKey = ""
 var WorkerAllowHttpImageRequestEnabled = false
